3ed302996008c81ec93abaced7d86a5afbfdb18a
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-02-19 15:05:34 -0300

    use descendants pattern for step component and add checkout label

--- react/Step.tsx
+++ react/Step.tsx
@@ -1,24 +1,20 @@
-import React, { useEffect, useRef, useReducer } from 'react'
+import React, { useLayoutEffect, useRef } from 'react'
 
 import { useStepContext } from './StepGroup'
 
 const useStepIndicator = (elementRef: React.RefObject<HTMLLIElement>) => {
-  const indexRef = useRef<number | null>(null)
-  const { registerStep, stepListRef } = useStepContext()
-  const [, forceUpdate] = useReducer((value: number) => value + 1, 0)
+  const indexRef = useRef<number>(-1)
+  const { assigning, items } = useStepContext()
 
-  useEffect(() => {
-    const { index, unregister } = registerStep(elementRef.current!)
-
-    indexRef.current = index
-    forceUpdate()
-
-    return () => {
-      unregister()
+  useLayoutEffect(() => {
+    if (assigning.current) {
+      indexRef.current = items.current.push(elementRef.current!) - 1
     }
-  }, [elementRef, registerStep])
+  })
 
-  return { index: indexRef.current, lastIndex: stepListRef.current.length }
+  // first render its wrong, after a forceUpdate in parent useLayoutEffect it's
+  // right, and its all synchronous so we don't get any flashing
+  return { index: indexRef.current, lastIndex: items.current.length - 1 }
 }
 
 interface StepProps {
@@ -33,7 +29,7 @@ const Step: React.FC<StepProps> = ({ children, title }) => {
     <li className="flex" ref={elementRef}>
       <div className="flex flex-column">
         <span className="w1 h1 pa2 bg-muted-1 br-100 c-on-muted-2 f5 b flex items-center justify-center">
-          {index}
+          {index + 1}
         </span>
         {(index ?? 0) < lastIndex && (
           <div
--- react/StepGroup.tsx
+++ react/StepGroup.tsx
@@ -1,20 +1,46 @@
-import React, { useMemo, useCallback, useRef, useContext } from 'react'
+import React, {
+  useMemo,
+  useRef,
+  useContext,
+  useLayoutEffect,
+  useCallback,
+  useState,
+} from 'react'
 import { useCssHandles } from 'vtex.css-handles'
 
+/**
+ * The component below is implemented following the "guidelines" of
+ * this [1] gist. It is known that creating compound components in React
+ * that have a strong relation of parent-child (like a select and their
+ * option's) is hard, and there is currently no good way to implement this
+ * because of the lack of primitives in the library.
+ *
+ * [1] https://gist.github.com/ryanflorence/10e9387f633f9d2e6f444a9bddaabf6e
+ */
+
 interface StepContext {
-  registerStep: (
-    element: HTMLLIElement
-  ) => { index: number; unregister: () => void }
-  stepListRef: React.MutableRefObject<HTMLLIElement[]>
+  items: React.MutableRefObject<HTMLLIElement[]>
+  assigning: React.MutableRefObject<boolean>
 }
 
 const ctx = React.createContext<StepContext | undefined>(undefined)
+
+const useForceUpdate = (): [object, () => void] => {
+  const [sentinel, setSentinel] = useState<object>({})
+
+  return [
+    sentinel,
+    useCallback(() => {
+      setSentinel({})
+    }, []),
+  ]
+}
 
 export const useStepContext = () => {
   const value = useContext(ctx)
 
   if (!value) {
-    throw new Error('useStepContext must be used inside a StepGroup')
+    throw new Error('useStepContext must be used inside a <StepGroup />')
   }
 
   return value
@@ -23,34 +49,55 @@ export const useStepContext = () => {
 const classes = ['stepGroupWrapper', 'stepGroupList']
 
 const StepGroup: React.FC = ({ children }) => {
+  const assigning = useRef(true)
+
+  const [sentinel, forceUpdate] = useForceUpdate()
   const stepListRef = useRef<HTMLLIElement[]>([])
+
   const cssHandles = useCssHandles(classes)
 
-  const registerStep = useCallback((element: HTMLLIElement) => {
-    const index = stepListRef.current.push(element)
-
-    return {
-      index,
-      unregister: () => {
-        const indexToPop = stepListRef.current.indexOf(element)
-
-        stepListRef.current.splice(indexToPop, 1)
-      },
+  useLayoutEffect(() => {
+    if (assigning.current) {
+      // At this point all of the children have pushed into the array so we set
+      // assigning to false and force an update. Since we're in
+      // useLayoutEffect, we won't get a flash of rendered content, it will all
+      // happen synchronously. And now that this is false, children won't push
+      // into the array on the forceUpdate      assigning.current = false
+      assigning.current = false
+      forceUpdate()
+    } else {
+      // After the forceUpdate completes, we end up here and set assigning back
+      // to true for the next update from the app
+      assigning.current = true
     }
-  }, [])
+
+    return () => {
+      // this cleanup function runs right before the next render, so it's the
+      // right time to empty out the array to be reassigned with whatever shows
+      // up next render.
+      if (assigning.current) {
+        // we only want to empty out the array before the next render cycle if
+        // it was NOT the result of our forceUpdate, so being guarded behind
+        // assigning.current works
+        stepListRef.current = []
+      }
+    }
+  })
 
   const contextValue = useMemo(
     () => ({
-      registerStep,
-      stepListRef,
+      items: stepListRef,
+      assigning,
+      sentinel,
     }),
-    [registerStep]
+    [sentinel]
   )
 
   return (
     <ctx.Provider value={contextValue}>
-      <div className={cssHandles.stepGroupWrapper}>
-        <ol className={cssHandles.stepGroupList}>{children}</ol>
+      <div className={`${cssHandles.stepGroupWrapper} ph3`}>
+        <h3 className="c-muted-1 f5 mb5 mt0">Checkout</h3>
+        <ol className={`${cssHandles.stepGroupList} pa0 ma0`}>{children}</ol>
       </div>
     </ctx.Provider>
   )
