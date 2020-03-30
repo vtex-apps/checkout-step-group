8c7e7623f3c454423a5d76bd1ff3dc41914b621a
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-03-30 13:37:43 -0300

    fix padding of step content

--- react/Step.tsx
+++ react/Step.tsx
@@ -105,14 +105,22 @@ const Step: React.FC<StepProps> = ({
       <div
         className={classNames(
           handles.stepContentWrapper,
-          'mt4 mb6 mb7-ns w-100 flex'
+          'mb6 mb7-ns w-100 flex',
+          {
+            'mt4 mt3-ns': !active,
+            'mt4 mt5-ns': active,
+          }
         )}
       >
         <div
           style={{ width: (index ?? 0) < lastIndex ? 1 : 0 }}
           className={classNames(
             handles.stepDivider,
-            'dn db-ns bg-muted-4 nt4 nb6 nb7-ns mh5'
+            'dn db-ns bg-muted-4 nb6 nb7-ns mh5',
+            {
+              'nt4 nt3-ns': !active,
+              'nt4 nt5-ns': active,
+            }
           )}
         />
         <div className={classNames(handles.stepContent, 'ml5-ns w-100 pr8-ns')}>
