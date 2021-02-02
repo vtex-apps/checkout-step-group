bcc3a6d8f2526e3a6bfa9b5eedd47ee0792d88df
Lucas Cordeiro <ecdb.lucas@gmail.com>
2021-02-02 15:18:04 -0300

    Ask for user login when editing address step

--- react/AddressStep.tsx
+++ react/AddressStep.tsx
@@ -3,16 +3,30 @@ import { FormattedMessage } from 'react-intl'
 import { ButtonPlain, IconEdit } from 'vtex.styleguide'
 import { Router, ContainerContext, routes } from 'vtex.checkout-container'
 import { AddressSummary, ShippingAddress } from 'vtex.checkout-shipping'
+import { OrderForm } from 'vtex.order-manager'
+import { OrderShipping } from 'vtex.order-shipping'
 
 import Step from './Step'
 
+const { useOrderForm } = OrderForm
+const { useOrderShipping } = OrderShipping
 const { useHistory, useRouteMatch } = Router
 const { useCheckoutContainer } = ContainerContext
 
 const AddressStep: React.FC = () => {
   const history = useHistory()
   const match = useRouteMatch(routes.ADDRESS)
-  const { isAddressEditable } = useCheckoutContainer()
+  const { requestLogin, isAddressEditable } = useCheckoutContainer()
+  const { orderForm } = useOrderForm()
+  const { selectedAddress } = useOrderShipping()
+
+  const handleAddressEdit = () => {
+    if (orderForm.canEditData || selectedAddress?.isDisposable) {
+      history.push(routes.ADDRESS)
+    } else {
+      requestLogin()
+    }
+  }
 
   return (
     <Step
@@ -21,7 +35,7 @@ const AddressStep: React.FC = () => {
       actionButton={
         !match &&
         isAddressEditable && (
-          <ButtonPlain onClick={() => history.push(routes.ADDRESS)}>
+          <ButtonPlain onClick={handleAddressEdit}>
             <IconEdit solid />
           </ButtonPlain>
         )
